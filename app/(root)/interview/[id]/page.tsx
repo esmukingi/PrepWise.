036d925a27bd7698923c0cdacc4deb078adc46c0
Richard <apple@Rich-2.local>
2025-04-22 02:36:36 +0200

    Pushing semi-final

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -5,35 +5,42 @@ import { getInterviewById } from '@/lib/actions/general.action';
 import { getRandomInterviewCover } from '@/lib/utils';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
-import React from 'react'
+import React from 'react';
 
-const page = async ({params}: RouteParams) => {
-    const {id} = await params;
-    const user = getCurrentUser();
-    const interview = await getInterviewById(id);
+const page = async ({ params }: RouteParams) => {
+  const { id } = params;
+  const user = await getCurrentUser();
+  const interview = await getInterviewById(id);
 
-    if(!interview) redirect('/');
+  if (!interview) redirect('/');
+
   return (
     <>
-    <div className='flex flex-row gap-4 justify-between'>
-        <div className='flex flex-col gap-4 items-center max=sm:flex-col'>
-            <div className='flex flex-row gap-4 items-center'>
-                <Image src={getRandomInterviewCover()} alt='cover-image' width={40} height={40} className='rounded-full object-cover size-[40px]'/>
-                <h3 className='capitalize'>{interview.role} Interview</h3>
-            </div>
-            <DisplayTechIcons techStack={interview.techstack}/>
+      <div className='flex flex-row gap-4 justify-between'>
+        <div className='flex flex-col gap-4 items-center max-sm:flex-col'>
+          <div className='flex flex-row gap-4 items-center'>
+            <Image
+              src={getRandomInterviewCover()}
+              alt='cover-image'
+              width={40}
+              height={40}
+              className='rounded-full object-cover size-[40px]'
+            />
+            <h3 className='capitalize'>{interview.role} Interview</h3>
+          </div>
+          <DisplayTechIcons techStack={interview.techstack} />
         </div>
         <p className='bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize'>{interview.type}</p>
-    </div>
-    <Agent 
-    userName={user?.name}
-    type={user?.id}
-    interviewId={id}
-    type='interview'
-    questions={interview.questions}
-    />
+      </div>
+      <Agent
+        userName={user?.name}
+        userId={user?.id}
+        interviewId={id}
+        type='interview'
+        questions={interview.questions}
+      />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { vapi } from '@/lib/vapi.sdk';
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action';
 enum CallStatus {
     INACTIVE = "INACTIVE",
     CONNECTING = "CONNECTING",
@@ -57,12 +58,12 @@ const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => 
     }, []);
     const handleGenerateFeedback = async (messages: SavedMessage[]) =>{
         console.log('Generate Feedback here.');
-
-        const {sucess, id} = {
-            sucess: true,
-            id: "feedback-id"
-        }
-        if(sucess && id){
+        const { success, feedbackId: id } = await createFeedback({
+            interviewId: interviewId!,
+            userId: userId!,
+            transcript: messages
+        });
+        if(success && id){
             router.push(`/interview/${interviewId}/feedback`)
         }
         else{
