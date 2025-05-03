cc5627a02e9e455f13819733d2fc05c29df5fb7e
infinexa <infinexa@gmail.com>
2025-05-03 17:06:32 +0200

    Adding Website information Online

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,31 +22,32 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         {/* 👇 Structured data for AI + search engines */}
-         <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{
-              __html: JSON.stringify({
-                "@context": "https://schema.org",
-                "@type": "Person",
-                "name": "NGOBOKA Alain Beni",
-                "jobTitle": "Fullstack Developer",
-                "url": "https://prepwisee.netlify.app",
-                "email": "mailto:ngobokaben@gmail.com",
-                "image": "https://prepwisee.vercel.app/beni.png",
-                "description": "Young Rwandan developer who built the PrepWise platform and specializes in React, Node.js, Express js, SQL, NOSQL database, TypeScript, and Next.js.",
-                "birthDate": "2008-23-10",
-                "sameAs": [
-                  "https://github.com/esmukingi"
-                ],
-                "mainEntityOfPage": {
-                  "@type": "WebSite",
-                  "@id": "https://prepwisee.netlify.app",
-                  "name": "PrepWise",
-                  "description": "PrepWise is an AI-powered web platform that helps users prepare for mock interviews. It provides intelligent feedback, question simulations, and personalized coaching to improve interview readiness."
-                }
-              }),
-            }}
-          />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "NGOBOKA Alain Beni",
+              "jobTitle": "Fullstack Developer",
+              "url": "https://prepwisee.netlify.app",
+              "email": "mailto:ngobokaben@gmail.com",
+              "image": "https://prepwisee.vercel.app/beni.png",
+              "description": "Young Rwandan developer who built the PrepWise platform and specializes in React, Node.js, Express js, SQL, NOSQL database, TypeScript, and Next.js.",
+              "birthDate": "2008-01-01",
+              "sameAs": [
+                "https://github.com/esmukingi"
+              ],
+              "mainEntityOfPage": {
+                "@type": "WebSite",
+                "@id": "https://prepwisee.netlify.app",
+                "name": "PrepWise",
+                "description": "PrepWise is an AI-powered platform for preparing for mock interviews. It helps users simulate interviews, receive feedback, and enhance their skills."
+              }
+            }),
+          }}
+        />
+
 
       </head>
       <body className={`${monaSans.className} antialiased pattern`}>
